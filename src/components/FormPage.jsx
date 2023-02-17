01fba0c041b5f7171bbd23c0f76a80e351835f22
BartoszJS <bartosz290300@wp.pl>
2023-02-17 23:55:40 +0100

    15%

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 const FormPage = () => {
   const fetchData = async () => {
     try {
-      const response = await fetch("http://localhost:5001/");
+      const response = await fetch("/api/v1");
       const data = await response.json();
       console.log(data);
     } catch (error) {
