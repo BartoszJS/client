2f8fed8636f307140689ddd2043d0ac4a3d883d8
BartoszJS <bartosz290300@wp.pl>
2023-02-19 23:19:58 +0100

    continue 60%

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -1,20 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { initialState, useAppContext } from "../context/appContext";
 
 const FormPage = () => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/api/v1");
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { name, price, image, createProduct } = useAppContext();
+  const [values, setValues] = useState(initialState);
 
-  return <div>FormPage</div>;
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch("/api/v1");
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handlePhoto = (e) => {
+    setValues({ ...values, image: e.target.files[0].name });
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <form
+        className='flex flex-col max-w-xl mx-auto space-y-2 pt-24'
+        onSubmit={onSubmit}
+      >
+        <input
+          className='border'
+          onChange={handleChange}
+          type='text'
+          value={name}
+          name='name'
+        />
+        <input
+          className='border border-black'
+          onChange={handleChange}
+          type='text'
+          value={price}
+          name='price'
+        />
+        <input onChange={handlePhoto} type='file' value={image} name='image' />
+        <button className='bg-red-300' type='submit'>
+          YAS
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default FormPage;
---
+++ src/context/reducer.js
@@ -0,0 +1,11 @@
+import {
+  CREATE_PRODUCT_BEGIN,
+  CREATE_PRODUCT_SUCCESS,
+  CREATE_PRODUCT_ERROR,
+} from "./actions";
+
+const reducer = (state, action) => {
+  throw new Error("error");
+};
+
+export default reducer;
